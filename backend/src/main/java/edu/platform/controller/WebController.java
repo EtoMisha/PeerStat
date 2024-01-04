f82b2dfd5e76ec00b5ce456d590eb552442ac70b
inigosto <demulcoder@gmail.com>
2024-01-04 21:16:56 +0300

    fix prev

--- backend/src/main/java/edu/platform/controller/WebController.java
+++ backend/src/main/java/edu/platform/controller/WebController.java
@@ -7,10 +7,7 @@ import edu.platform.service.ProjectService;
 import edu.platform.service.UserProjectService;
 import edu.platform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
